57bac0b0919d7cae4ee3364123d4ef2d463cf478
jacobesplin <39013325+jacobesplin@users.noreply.github.com>
2021-08-24 09:33:48 -0600

    Making changes to git!

---
+++ FirstAssignment/src/FirstClass.java
@@ -0,0 +1,14 @@
+package src;
+
+public class FirstClass {
+	
+	private static void print(Object obj) {
+		System.out.println(obj);
+	}
+	
+	
+	public static void main(String[] args) {
+		print("Hello world!");
+	}
+
+}
